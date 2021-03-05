c0558897822e0567185ddad100f658512f3f498b
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-03-05 10:45:25 +0000

    improve logging

--- main.py
+++ main.py
@@ -6,53 +6,58 @@ import pickle
 import numpy as np
 import tqdm
 
+
 def parse_arguments():
     parser = ArgumentParser()
+    parser.add_argument('-N', type=int, default=1, help='number of subjects')
+    parser.add_argument('-n', type=int, default=10,
+                        help='number of observations')
     parser.add_argument('-k', type=int, default=5, help='latent dim')
-    parser.add_argument('-p', type=int, default=10, help='observation dim')
-    parser.add_argument('-i', '--n-iters', type=int, default=250, help='number of iterations')
+    parser.add_argument('-p', type=int, default=50, help='observation dim')
+    parser.add_argument('-i', '--n-iters', type=int,
+                        default=250, help='number of iterations')
     return parser.parse_args()
-
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    n = 1000
-    N = 1
+    n = args.n
+    N = args.N
     p = args.p
     k = args.k
     n_iters = args.n_iters
 
-    N_vals = [1, 2, 3, 5, 10]
-    n_vals = [10, 25, 50, 100, 150, 250, 500, 1000, 1500, 2500]
+    res = {'jaccard': [], 'hamming': [], 'kulsinski': [],
+           'score': [], 'cov_mse': {i: [] for i in range(N)}}
+    res_leg = {'jaccard': [], 'hamming': [], 'kulsinski': [],
+               'score': [], 'cov_mse': {i: [] for i in range(N)}}
+
     distances = ['jaccard', 'hamming', 'kulsinski']
 
-    res = {N : {n: {d: [] for d in distances} for n in n_vals} for N in N_vals}
-    res_leg = {N : {n: {d: [] for d in distances} for n in n_vals} for N in N_vals}
+    print(f"Running for {N} subjects and {n} observations")
+    for iteration in tqdm.tqdm(range(n_iters)):
+        X, Z, W, G = generate_all(n, N, p, k, seed=iteration)
+        covs = [np.cov(x, rowvar=False) for x in X]
 
-    for N in N_vals:
-        for n in n_vals:
-            print(f"Running for {N} subjects and {n} observations")
-            for iteration in tqdm.tqdm(range(n_iters)):
-                X, Z, W, G = generate_all(n, N, p, k, ones=True, seed=iteration)
-                covs = [np.cov(x, rowvar=False) for x in X]
+        model = MHA(k=k)
+        model.fit(X=X, max_iter=5000)
+        cs = cluster_score(model.W, W)
+        for d in distances:
+            res[d].append(cs[0][d])
+        res['score'].append(cs[1])
+        for in in range(N):
+            res['cov_mse'][i].append(covariance_mse(model.G[i], G[i]))
 
-                model = MHA(k=k)
-                model.fit(X=X, max_iter=5000)
-                for d in distances:
-                    res[N][n][d].append(cluster_score(model.W, W, distance=d))
-                res[N][n]['cov_mse'] = []
-                for i in range(N):
-                    res[N][n]['cov_mse'].append(covariance_mse(model.G[i], G[i]))
+        model_leg = LegacyMHA(covs, k=k)
+        model_leg.fit(maxIter=5000)
+        cs = cluster_score(model_leg.W, W)
+        for d in distances:
+            res_leg[d].append(cs[0][d])
+        res_leg['score'].append(cs[1])
+        for in in range(N):
+            res_leg['cov_mse'][i].append(covariance_mse(model_leg.G[i], G[i]))
 
-                model_leg = LegacyMHA(covs, k=k)
-                model_leg.fit(maxIter=5000)
-                for d in distances:
-                    res_leg[N][n][d].append(cluster_score(model_leg.W, W, distance=d))
-                res_leg[N][n]['cov_mse'] = []
-                for i in range(N):
-                    res_leg[N][n]['cov_mse'].append(covariance_mse(model_leg.G[i], G[i]))
-
-    pickle.dump(res, open(f'results_{n_iters}.p', 'wb'))
-    pickle.dump(res_leg, open(f'results_leg_{n_iters}.p', 'wb'))
-
+    pickle.dump(res,
+                open(f'results/results_{N}_{n}_{p}_{k}_{n_iters}.p', 'wb'))
+    pickle.dump(res_leg,
+                open(f'results/results_leg_{N}_{n}_{p}_{k}_{n_iters}.p', 'wb'))
--- metrics.py
+++ metrics.py
@@ -4,7 +4,7 @@ from scipy.optimize import linear_sum_assignment
 from scipy.spatial import distance as spd
 
 
-def cluster_score(W, W_true, distance='jaccard'):
+def cluster_score(W, W_true):
     """
     Permute the columns of the estimate W in order to be aligned with the columns of W_true.
     This is because the model is invariant to permutations of the columns of W.
@@ -16,23 +16,19 @@ def cluster_score(W, W_true, distance='jaccard'):
         Estimated loading matrix
     W_true: np.ndarray
         True loading matrix
-    distance: str
-        Distance to compute between the true and the estimated clusters.
-        Can be either `jaccard`, `hamming` or `kulsinski`.
 
     Returns:
     ----------
+    distances: dict
+        The distance between the true and the estimated clusters.
+        Compute the "Jaccard", "Hamming" and "Kulsinski" distances and return them as a dict.
     score: float
         The optimal assignment cost.
     W_aligned: np.ndarray
         A copy of W with its columns rearranged according to the optimal alignment.
-    dist: float
-        The distance between the true and the estimated clusters.
     alignment: tuple of (np.ndarray, np.ndarray)
         The row-idx and column-idx of the optinal alignment
     """
-    # process distance argument
-    assert distance in ['jaccard', 'hamming', 'kulsinski']
     # compute cost matrix
     cost = np.sum((W[:, :, None] - W_true[:, None, :]) ** 2, axis=0)
     # solve the assignment problem
@@ -43,13 +39,11 @@ def cluster_score(W, W_true, distance='jaccard'):
     W_aligned = W.copy()[:, alignment[1]]
     # compare clusters
     am_W, am_W_true = W_aligned.argmax(1), W_true.argmax(1)
-    if distance == 'jaccard':
-        dist = spd.jaccard(am_W, am_W_true)
-    elif distance == 'hamming':
-        dist = spd.hamming(am_W, am_W_true)
-    else:
-        dist = spd.kulsinski(am_W, am_W_true)
-    return score, W_aligned, dist, alignment
+    distances = {}
+    distances['jaccard'] = spd.jaccard(am_W, am_W_true)
+    distances['hamming'] = spd.hamming(am_W, am_W_true)
+    distances['kulsinski'] = spd.kulsinski(am_W, am_W_true)
+    return distances, score, W_aligned, alignment
 
 
 def covariance_mse(Gi, Gi_true):
