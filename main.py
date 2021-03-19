88b09df2a3c2cacccef8236019341cb8975f67b5
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-03-19 10:23:50 +0000

    add time_complexity

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+import sys
+import time
+import pickle
+from argparse import ArgumentParser
+import numpy as np
+from tqdm import tqdm
 from mha import MHA, LegacyMHA, project_W
 from data.generate_synth_data import generate_all
-from argparse import ArgumentParser
 from metrics import cluster_score, covariance_mse
-import pickle
-import numpy as np
-import tqdm
 
 
 def parse_arguments():
@@ -16,16 +19,14 @@ def parse_arguments():
     parser.add_argument('-p', type=int, default=50, help='observation dim')
     parser.add_argument('-i', '--n-iters', type=int,
                         default=250, help='number of iterations')
+    parser.add_argument('-r', '--run', type=str,
+                        default='results', help='Dir to save results')
+    parser.add_argument('-t', '--time', action='store_true',
+                        help='run time complexity')
     return parser.parse_args()
 
 
-if __name__ == "__main__":
-    args = parse_arguments()
-    n = args.n
-    N = args.N
-    p = args.p
-    k = args.k
-    n_iters = args.n_iters
+def main_sim(N, n, p, k, n_iters, run_dir):
 
     res = {'jaccard': [], 'hamming': [], 'kulsinski': [], 'score': [],
            'ha_jaccard': [], 'ha_hamming': [], 'ha_kulsinski': [], 'ha_score': [],
@@ -37,12 +38,12 @@ if __name__ == "__main__":
     distances = ['jaccard', 'hamming', 'kulsinski']
 
     print(f"Running for {N} subjects and {n} observations")
-    for iteration in tqdm.tqdm(range(n_iters)):
+    for iteration in tqdm(range(n_iters)):
         X, Z, W, G = generate_all(n, N, p, k, seed=iteration)
         covs = [np.cov(x, rowvar=False) for x in X]
 
-        model = MHA(k=k, init_method='sparse_svd')
-        model.fit(X=X, max_iter=5000)
+        model = MHA(k=k)
+        model.fit(X=X, max_iter=5000, init_method='sparse_svd')
         cs = cluster_score(model.W, W)
         for d in distances:
             res[d].append(cs[0][d])
@@ -68,6 +69,38 @@ if __name__ == "__main__":
             res_leg['cov_mse'][i].append(covariance_mse(model_leg.G[i], G[i]))
 
     pickle.dump(res,
-                open(f'results/results_{N}_{n}_{p}_{k}_{n_iters}.p', 'wb'))
+                open(f'{run_dir}/results_{N}_{n}_{p}_{k}_{n_iters}.p', 'wb'))
     pickle.dump(res_leg,
-                open(f'results/results_leg_{N}_{n}_{p}_{k}_{n_iters}.p', 'wb'))
+                open(f'{run_dir}/results_leg_{N}_{n}_{p}_{k}_{n_iters}.p', 'wb'))
+
+
+def time_complexity(n, p, k, n_iters, run_dir):
+    N_vals = range(1, 21)
+    res = {N: [] for N in N_vals}
+
+    for N in N_vals:
+        for it in tqdm(range(n_iters)):
+            X, Z, W, G = generate_all(n, N, p, k, seed=it)
+            st = time.time()
+            model = MHA(k=k)
+            model.fit(X=X, max_iter=5000, init_method='sparse_svd')
+            duration = time.time() - st
+            res[N].append(duration)
+
+    pickle.dump(res, open(f'{run_dir}/time_{n}_{p}_{k}_{n_iters}.p', 'wb'))
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    n = args.n
+    N = args.N
+    p = args.p
+    k = args.k
+    n_iters = args.n_iters
+    run_dir = args.run
+    os.makedirs(run_dir, exist_ok=True)
+    rt = args.time
+    if not rt:
+        sys.exit(main_sim(N, n, p, k, n_iters, run_dir))
+    else:
+        sys.exit(time_complexity(n, p, k, n_iters, run_dir))
